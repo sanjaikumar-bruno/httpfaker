91402f4a676357de2b45c8b1972b012e5a38edaf
Anoop M D <anoop.md1421@gmail.com>
2025-03-12 22:52:15 +0530

    feat: bruno collection

--- src/index.js
+++ src/index.js
@@ -20,12 +20,12 @@ app.use(bodyParser.urlencoded({ extended: true, verify: saveRawBody }));
 app.use(bodyParser.text({ verify: saveRawBody }));
 app.use(express.raw({ type: '*/*', limit: '100mb', verify: saveRawBody }));
 
-// Serve OpenAPI documentation
-app.use('/', swaggerUi.serve, swaggerUi.setup(openApiSpecification));
-
 app.post('/api/echo', echo);
 app.post('/api/echo/raw', echoRaw);
 app.post('/api/echo/custom', echoCustom);
+
+app.get('/', swaggerUi.serve, swaggerUi.setup(openApiSpecification));
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
